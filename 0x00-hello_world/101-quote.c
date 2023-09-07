c1f304e6918729642e2169009b16cf424615dcf0
Eagleine <millizo@yahoo.com>
2023-09-07 16:53:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unisted>
 /**
  * main - A c program that prints a new to standard error
  * Return : 1 (success)
@@ -6,5 +7,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-	return (1);
+return (1);
 }
